21ced202178a6504fc507dcf08c7affba3c5e509
iameugn <metajjuxhin@gmail.com>
2023-10-11 17:46:28 +0200

    Finalized main project

--- crawlerTrigger.py
+++ crawlerTrigger.py
@@ -2,6 +2,10 @@ import json
 from newsFetcher import get_news_headlines
 from newsScraperSummarizer import newsScraper
 import htmlGenerator
+import os
+
+# Determine the full path to the templates folder
+templates_folder = os.path.join(os.path.dirname(__file__), 'templates')
 
 # Get the news list from the scraper
 headlines = get_news_headlines()
@@ -13,7 +17,5 @@ with open ("data.json", "w") as f:
 data_json_file = 'data.json'
 html_code = htmlGenerator.json2Html(data_json_file)
 
-# Write the HTML code to a file
-with open("allNews.html", "w", encoding='utf-8') as file:
+with open(os.path.join(templates_folder, 'allNews.html'), 'w', encoding='utf-8') as file:
     file.write(html_code)
-    
--- htmlGenerator.py
+++ htmlGenerator.py
@@ -6,47 +6,125 @@ def json2Html(json_file):
     num_objects = len(articles)
 
     html = f'''
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>News Articles</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="static/style.css">
-        <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
-        <style>
-            /* Define the 'expanded' class to initially hide content */
-            
-        </style>
-    </head>
-    <body>
-        <h1 id="title">Articles {num_objects}</h1>
-        <div class="content">
+     <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Quick news </title>
+  <link rel="stylesheet" href="https://unpkg.com/bulma@0.9.4/css/bulma.min.css" />
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link rel="stylesheet" href="../css/bulma-divider.min.css">
+
+<link rel="stylesheet" href="static/css/style.css">
+</head>
+
+<body>
+  <!-- START NAV -->
+  <nav class="navbar">
+    <div class="container">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="../">
+                <img style="width:100px!important;height:auto;max-height:100px;" src="static/images/logo.png" alt="Logo">
+            </a>
+            <span class="navbar-burger burger" data-target="navbarMenu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+        </div>
+        <div id="navbarMenu" class="navbar-menu">
+            <div class="navbar-end">
+                <span class="navbar-item">
+                    <a class="button" href="../" style="color: #141E46;">
+                        <span class="icon">
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                        </span>
+                        <span>Home</span>
+                    </a>
+                </span>
+
+            </div>
+        </div>
+    </div>
+</nav>
+  <!-- END NAV -->
+<h1 class="title" style="text-align:center;">Articles {num_objects}</h1>
+  <!-- Image -->
+  <section class="hero ">
+
+        
+        
     '''
 
     for article in articles:
         html += f'''
-            <div class="entire-article">
-            <div class="lefty">
-                <h2><a href="{article['Url']}">{article['Title']}</a></h2>
+            
+            
+            
+            <section class="section">
+          <div class="columns">
+            <div class="column is-8 is-offset-2">
+              <div class="content is-medium">
+                
+                <h2 class="title"><a href="{article['Url']}">{article['Title']}</a></h2>
                 <span class="link">--{article['Source']}</span>
+                <h3 class="subtitle is-4" onclick="toggleExpand(this)">{article['Summary']}</h3>
+                <p class="element-article">{article['Content']}</p></div></div>
+              </div>
             </div>
-            <div class="righty">
-            <p class="summary">{article['Summary']}</p>
-            <span class="element-title" onclick="toggleExpand(this)">Read</span>
-            <p class="element-article">{article['Content']}</p></div></div>
+          </div>
+        </section>
+
+        <div class="is-divider"></div>
         '''
 
     html += '''
-      </div>
-        <script>
-            // JavaScript to toggle the 'expanded' class on click
+       </div>
+    </div>
+  </section>
+
+
+  <script>
+    document.addEventListener('DOMContentLoaded', () => {
+
+      // Get all "navbar-burger" elements
+      const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+      // Check if there are any navbar burgers
+      if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(el => {
+          el.addEventListener('click', () => {
+
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+
+          });
+        });
+      }
+
+    });
+    
+       // JavaScript to toggle the 'expanded' class on click
             function toggleExpand(element) {
                 var articleElement = element.nextElementSibling;
                 articleElement.classList.toggle("expanded");
             }
-        </script>
-    </body>
-    </html>
+  </script>
+
+</body>
+
+</html>
     '''
 
     return html
